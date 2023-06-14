b4ba82ad05120b9cc6b6828c180437cb3131f7b1
vanessaperaltav <125827890+vanessaperaltav@users.noreply.github.com>
2023-06-13 23:58:12 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -7,27 +7,26 @@ correctamente. Tenga en cuenta datos faltantes y duplicados.
 
 """
 import pandas as pd
-
-
+import re
+from datetime import datetime
 def clean_data():
 
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
+    df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col=0)
 
-    df = df.drop('Unnamed: 0', axis = 1)
-    df.fecha_de_beneficio = pd.to_datetime(df.fecha_de_beneficio, dayfirst=True)
-
-    df.monto_del_credito = df.monto_del_credito.apply(lambda x: str(x).strip('$').replace(',',''))
+    df.dropna(axis=0, inplace=True)
+    df.monto_del_credito = df.monto_del_credito.str.strip('$')
+    df.monto_del_credito = df.monto_del_credito.str.replace(',','') 
     df.monto_del_credito = df.monto_del_credito.astype(float)
     df.monto_del_credito = df.monto_del_credito.astype(int)
-
-    for columna in ['sexo','tipo_de_emprendimiento',
-                    'idea_negocio','barrio','línea_credito']:
-        df[columna] = df[columna].apply(
-                                        lambda s: s.lower().replace('-',' ').replace('_',' ') 
-                                        if type(s) == str else s)
-    df.dropna(axis=0, inplace = True)
+    df.fecha_de_beneficio = df.fecha_de_beneficio.apply(lambda x: datetime.strptime(x, "%Y/%m/%d") if (len(re.findall("^\d+/", x)[0]) - 1) == 4 else datetime.strptime(x, "%d/%m/%Y"))  
+    df.comuna_ciudadano = df.comuna_ciudadano.apply(float)
+    #colString = df.select_dtypes(include=['object']).columns.tolist()
+    
+    for i in ['sexo', 'tipo_de_emprendimiento', 'idea_negocio', 'línea_credito', 'barrio']:
+        df[i] = df[i].str.lower()
+        df[i] = df[i].str.replace('_',' ')
+        df[i] = df[i].str.replace('-',' ')
+    
     df.drop_duplicates(inplace=True)
 
     return df
-
-print(clean_data().sexo.value_counts().to_list())
