80d2cdf718c5fdb5651e08938a248169f8801e79
vanessaperaltav <125827890+vanessaperaltav@users.noreply.github.com>
2023-06-13 19:30:34 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -12,16 +12,22 @@ import pandas as pd
 def clean_data():
 
     df = pd.read_csv("solicitudes_credito.csv", sep=";")
-    df.dropna(axis = 0, inplace = True)
-    df.drop_duplicates(inplace = True)
-    df=df.drop(['Unnamed: 0'], axis=1)
-    df[["sexo", "tipo_de_emprendimiento","idea_negocio","barrio","línea_credito"]]=df[["sexo", "tipo_de_emprendimiento","idea_negocio","barrio","línea_credito"]].apply(lambda x: x.astype(str).str.lower())
-    df=df.replace(to_replace="(_)|(-)",value=" ",regex=True)    
-    df=df.replace(to_replace="[,$]|(\.00$)",value="",regex=True)
-    df.monto_del_credito = df.monto_del_credito.astype("int")
-    df.comuna_ciudadano = df.comuna_ciudadano.astype("float")
-    df.fecha_de_beneficio = pd.to_datetime(df.fecha_de_beneficio,infer_datetime_format=True,errors='ignore',dayfirst=True)
-    df.fecha_de_beneficio = df.fecha_de_beneficio.dt.strftime("%Y/%m/%d")
-    df.drop_duplicates(inplace = True)
+
+    df = df.drop('Unnamed: 0', axis = 1)
+    df.fecha_de_beneficio = pd.to_datetime(df.fecha_de_beneficio, dayfirst=True)
+
+    df.monto_del_credito = df.monto_del_credito.apply(lambda x: str(x).strip('$').replace(',',''))
+    df.monto_del_credito = df.monto_del_credito.astype(float)
+    df.monto_del_credito = df.monto_del_credito.astype(int)
+
+    for columna in ['sexo','tipo_de_emprendimiento',
+                    'idea_negocio','barrio','línea_credito']:
+        df[columna] = df[columna].apply(
+                                        lambda s: s.lower().replace('-',' ').replace('_',' ') 
+                                        if type(s) == str else s)
+    df.dropna(axis=0, inplace = True)
+    df.drop_duplicates(inplace=True)
 
     return df
+
+print(clean_data().sexo.value_counts().to_list())
